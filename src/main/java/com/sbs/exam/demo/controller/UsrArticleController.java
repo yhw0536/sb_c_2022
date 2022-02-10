1a4f0c00aab9960a7cda898423035df61f960fc1
yhw0536 <yhw0536@gmail.com>
2022-02-10 13:49:12 +0900

    getArticle, deleteArticle 서비스 메서드로 게시물 삭제기능 구현

--- src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/demo/controller/UsrArticleController.java
@@ -11,44 +11,61 @@ import com.sbs.exam.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
+	// 인스턴스 변수 시작
 	private int articlesLastId;
 	private List<Article> articles;
+	// 인스턴스 변수 끝
 
+	// 생성자
 	public UsrArticleController() {
 		articlesLastId = 0;
 		articles = new ArrayList<>();
 
 	}
+
+	// 서비스 메서드 시작
 	private void makeTestData() {
-		for (int i=1; i<=10; i++) {
-			
+		for (int i = 1; i <= 10; i++) {
+
 			String title = "제목 " + i;
 			String body = "내용 " + i;
-			
+
 			writeArticle(title, body);
-			
+
 		}
 	}
-	
+
 	public Article writeArticle(String title, String body) {
 		int id = articlesLastId + 1;
 		Article article = new Article(id, title, body);
-				
+
 		articles.add(article);
 		articlesLastId = id;
-		
-		return article;
-			}
-	
 
+		return article;
+	}
+
+	private Article getArticle(int id) {
+		for (Article article : articles) {
+			if (article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+	
+	private void deleteArticle(int id) {
+		Article article = getArticle(id);
+		
+		articles.remove(article);
+	}
+	// 서비스 메서드 끝
+	
+	// 액션 메서드 시작
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-		int id = articlesLastId + 1;
-		Article article = new Article(id, title, body);
-
-		articles.add(article);
-		articlesLastId = id;
+		Article article = writeArticle(title, body);
 
 		return article;
 	}
@@ -58,10 +75,20 @@ public class UsrArticleController {
 	public List<Article> getArticles() {
 		return articles;
 	}
-	
+
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {
+		Article article = getArticle(id);
+
+		if ( article == null ) {
+			return id + "번 게시물이 존재하지 않습니다.";			
+		}
+
+		deleteArticle(id);
+		
+		
 		return id + "번 게시물을 삭제했습니다.";
 	}
+	// 액션 메서드 끝
 }
